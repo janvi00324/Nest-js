4aac9e4020d44ade8e9af9e9c551eb8bedc3415e
janvi-tagline <jid@taglineinfotech.com>
2025-08-22 11:27:01 +0530

    Manage env variable in structured way

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import express from 'express';
+import morgan from 'morgan';
+import { appConfig } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(morgan(':method :url :response-time'));
+  app.use(express.json());
+  console.log('process.env.PORT', appConfig.PORT);
+  await app.listen(appConfig.PORT ?? 3000);
 }
 bootstrap();
